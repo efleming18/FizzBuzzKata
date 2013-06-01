ce1c1ecebcb79b6f462402af985c4663ec75138c
Rich <richhildebrand@gmail.com>
2013-06-01 10:18:53 -0400

    red - ReturnTrueWhenInputIsDivisibleByFive

--- FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -5,6 +5,12 @@ namespace FizzBuzzKata
 {
     public class FizzBuzz
     { 
+        public object IsDivisibleByFive(int p)
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+
         public bool IsDivisibleByThree(int input)
         {
             return input % 3 == 0;
---
+++ FizzBuzzKata/FizzBuzzKata/UnitTests/FizzBuzzTests/IsDivisibleByFiveShould.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.UnitTests.FizzBuzzTests
+{
+    [TestFixture]
+    class IsDivisibleByFiveShould
+    {
+        [Test]
+        public void ReturnTrueWhenInputIsDivisibleByFive()
+        {
+            // Yes! The app would be nice!
+            var fizzBuzz = new FizzBuzz();
+            var expectedValue = true;
+
+            var actualValue = fizzBuzz.IsDivisibleByFive(5);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
