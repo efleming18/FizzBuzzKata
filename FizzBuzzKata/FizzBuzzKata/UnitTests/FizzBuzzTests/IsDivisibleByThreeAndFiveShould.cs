497eec39dd7ff175d90e625b693b6e3945e70739
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-02 22:42:38 -0400

    refactor - Created SetUp method and removed usings

--- FizzBuzzKata/FizzBuzzKata/UnitTests/FizzBuzzTests/IsDivisibleByThreeAndFiveShould.cs
+++ FizzBuzzKata/FizzBuzzKata/UnitTests/FizzBuzzTests/IsDivisibleByThreeAndFiveShould.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace FizzBuzzKata.UnitTests.FizzBuzzTests
@@ -10,12 +7,19 @@ namespace FizzBuzzKata.UnitTests.FizzBuzzTests
     [TestFixture]
     class IsDivisibleByThreeAndFiveShould
     {
+        private FizzBuzz _fizzBuzz;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this._fizzBuzz = new FizzBuzz();
+        }
+
         [Test]
         public void ReturnTrueWhenInputIsDivisibleByThreeAndFive()
         {
-            var fizzBuzz = new FizzBuzz();
             var expectedValue = true;
-            var actualValue = fizzBuzz.IsDivisibleByThreeAndFiveShould(15);
+            var actualValue = _fizzBuzz.IsDivisibleByThreeAndFiveShould(15);
 
             Assert.AreEqual(expectedValue, actualValue);
         }
