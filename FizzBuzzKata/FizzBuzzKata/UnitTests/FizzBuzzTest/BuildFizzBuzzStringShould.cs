3878a13e3ffa89a348a6548f7c44935b51d840c5
Rich <richhildebrand@gmail.com>
2013-06-04 18:00:59 -0400

    Red - ReturnFizzIfInputIsDivisibleByThree
    woo, you get our first real refactor =D   Also, you would think github
    for windows would respect the word refactor.

--- FizzBuzzKata/FizzBuzzKata/UnitTests/FizzBuzzTest/BuildFizzBuzzStringShould.cs
+++ FizzBuzzKata/FizzBuzzKata/UnitTests/FizzBuzzTest/BuildFizzBuzzStringShould.cs
@@ -11,9 +11,20 @@ namespace FizzBuzzKata.UnitTests.FizzBuzzTest
         public void ReturnFizzIfInputIsDivisibleByThree()
         {
             var fizzBuzz = new FizzBuzz();
-
             string expectedValue = "Fizz";
+ 
             string actualValue = fizzBuzz.BuildFizzBuzzString(3);
+            //Contain Fizz? Not divisible by Five?
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [Test]
+        public void ReturnFizzBuzzIfInputIsDivisibleByThreeAndFive()
+        {
+            var fizzBuzz = new FizzBuzz();
+            string expectedValue = "FizzBuzz";
+
+            string actualValue = fizzBuzz.BuildFizzBuzzString(15);
 
             Assert.AreEqual(expectedValue, actualValue);
         }
