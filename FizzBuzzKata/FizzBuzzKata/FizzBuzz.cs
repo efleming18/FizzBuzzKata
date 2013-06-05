71d7e5726fd324c6c2c29c63d76e86449a698f56
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-04 23:46:17 -0400

    Green - ReturnFizzBuzzIfInputIsDivisibleByThreeAndFive

--- FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -5,8 +5,14 @@ namespace FizzBuzzKata
 {
     public class FizzBuzz
     { 
+        readonly Calculator _calculator = new Calculator();
+
         public string BuildFizzBuzzString(int input)
         {
+            if (_calculator.IsDivisibleByThreeAndFive(input))
+            {
+                return "FizzBuzz";
+            }
             return "Fizz";
         }
     }
