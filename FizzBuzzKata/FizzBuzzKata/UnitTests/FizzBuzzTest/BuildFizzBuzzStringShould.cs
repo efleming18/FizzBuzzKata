7c29bb198dc9822000d616890e0e24fa72f45021
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-04 23:52:09 -0400

    Refactor - Added SetUp method to BuildFizzBuzzStringShould

--- FizzBuzzKata/FizzBuzzKata/UnitTests/FizzBuzzTest/BuildFizzBuzzStringShould.cs
+++ FizzBuzzKata/FizzBuzzKata/UnitTests/FizzBuzzTest/BuildFizzBuzzStringShould.cs
@@ -7,24 +7,29 @@ namespace FizzBuzzKata.UnitTests.FizzBuzzTest
     [TestFixture]
     public class BuildFizzBuzzStringShould
     {
+        private FizzBuzz _fizzBuzz;
+        [SetUp]
+        public void SetUp()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
+
         [Test]
         public void ReturnFizzIfInputIsDivisibleByThree()
         {
-            var fizzBuzz = new FizzBuzz();
             string expectedValue = "Fizz";
- 
-            string actualValue = fizzBuzz.BuildFizzBuzzString(3);
-            //Contain Fizz? Not divisible by Five?
+
+            string actualValue = _fizzBuzz.BuildFizzBuzzString(3);
+
             Assert.AreEqual(expectedValue, actualValue);
         }
 
         [Test]
         public void ReturnFizzBuzzIfInputIsDivisibleByThreeAndFive()
         {
-            var fizzBuzz = new FizzBuzz();
             string expectedValue = "FizzBuzz";
 
-            string actualValue = fizzBuzz.BuildFizzBuzzString(15);
+            string actualValue = _fizzBuzz.BuildFizzBuzzString(15);
 
             Assert.AreEqual(expectedValue, actualValue);
         }
