96420f5f2bdd0442dc36c7a3659688b6dd87d367
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-12 19:03:02 -0400

    Red - ReturnTrueIfInputContainsFive

---
+++ FizzBuzzKata/FizzBuzzKata/UnitTests/StringInspectorTests/ContainsFiveShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.UnitTests.StringInspectorTests
+{
+    [TestFixture]
+    public class ContainsFiveShould
+    {
+        [Test]
+        public void ReturnTrueIfInputContainsFive()
+        {
+            var stringInspector = new StringInspector();
+            var expectedValue = true;
+
+            var actualValue = stringInspector.ContainsFive(15);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
