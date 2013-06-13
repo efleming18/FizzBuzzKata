85be315245019f78c9d3f3358f260623fef63161
Rich <richhildebrand@gmail.com>
2013-06-12 20:42:58 -0400

    Red - ReturnFalseIfInputDoesNotContainThree
    not sure how to do the integration tests though :(

--- FizzBuzzKata/FizzBuzzKata/UnitTests/StringInspectorTests/ContainThreeShould.cs
+++ FizzBuzzKata/FizzBuzzKata/UnitTests/StringInspectorTests/ContainThreeShould.cs
@@ -7,18 +7,35 @@ namespace FizzBuzzKata.UnitTests.StringInspectorTests
     [TestFixture]
     public class ContainThreeShould
     {
+        private StringInspector _stringInspector;
+
         [SetUp]
         public void SetUp()
         {
-            var _stringInspector = new StringInspector();
+            _stringInspector = new StringInspector();
+        }
 
         [Test]
         public void ReturnTrueIfInputContainsThree()
         {           
-            var stringInspector = new StringInspector();
             var expectedValue = true;
 
-            var actualValue = stringInspector.ContainsThree(31);
+            var actualValue = _stringInspector.ContainsThree(31);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+
+        [Test]
+        public void ReturnFalseIfInputDoesNotContainThree()
+        {
+            //I'm aware we have 4 broken tests but those will not pass until these tests are done but I like the idea of testing the whole application like that.
+            // Yes! I'm not sure how I want them written though.
+            // Too complex names loose meaning, and not enough complexity might be missleading. /shrug
+
+            var expectedValue = false;
+
+            var actualValue = _stringInspector.ContainsThree(99);
 
             Assert.AreEqual(expectedValue, actualValue);
         }
