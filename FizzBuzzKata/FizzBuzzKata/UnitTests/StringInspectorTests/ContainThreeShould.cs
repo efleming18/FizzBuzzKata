54a606d849e6a8969f17829c7d02e241ea461252
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-12 20:31:15 -0400

    Red - ReturnTrueIfInputContainsThree

---
+++ FizzBuzzKata/FizzBuzzKata/UnitTests/StringInspectorTests/ContainThreeShould.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.UnitTests.StringInspectorTests
+{
+    [TestFixture]
+    public class ContainThreeShould
+    {
+        [Test]
+        public void ReturnTrueIfInputContainsThree()
+        {
+            //I'm aware we have 4 broken tests but those will not pass until these tests are done but I like the idea of testing the whole application like that.
+            var stringInspector = new StringInspector();
+            var expectedValue = true;
+
+            var actualValue = stringInspector.ContainsThree(31);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
