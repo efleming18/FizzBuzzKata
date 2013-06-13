52751be668fd120772ba7eb2be079aa701090d5d
Rich <richhildebrand@gmail.com>
2013-06-13 17:37:30 -0400

    integration test

--- FizzBuzzKata/FizzBuzzKata/IntegrationTests/FizzBuzzShould.cs
+++ FizzBuzzKata/FizzBuzzKata/IntegrationTests/FizzBuzzShould.cs
@@ -17,10 +17,12 @@ namespace FizzBuzzKata.IntegrationTests
             _fizzBuzz = new FizzBuzz();
         }
 
-
-        //Theme to test if you have completed the kata? 
         [TestCase(9)]
         [TestCase(3)]
+        [TestCase(36)]
+        [TestCase(18)]
+        [TestCase(333)]
+        [TestCase(966)]        
         public void ReturnFizz(int input)
         {
             var expectedValue = "Fizz";
@@ -30,12 +32,72 @@ namespace FizzBuzzKata.IntegrationTests
             Assert.AreEqual(expectedValue, actualValue);
         }
 
-        //Or theme good test names - this could get scary in the bonus section?
-        [TestCase(9)]
-        [TestCase(3)]
-        public void ReturnFizzWhenInputIsDivisibleByThreeButNotFive(int input)
+        [TestCase(5)]
+        [TestCase(25)]
+        [TestCase(50)]
+        [TestCase(110)]
+        [TestCase(545)]
+        [TestCase(1070)]
+        public void ReturnBuzz(int input)
+        {
+            var expectedValue = "Buzz";
+
+            var actualValue = _fizzBuzz.BuildFizzBuzzString(input);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(60)]
+        [TestCase(135)]
+        [TestCase(555)]
+        [TestCase(1050)]
+        public void ReturnFizzBuzz(int input)
+        {
+            var expectedValue = "FizzBuzz";
+
+            var actualValue = _fizzBuzz.BuildFizzBuzzString(input);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+
+        [TestCase(13)]
+        [TestCase(32)]
+        [TestCase(137)]
+        [TestCase(3002)]
+        public void ReturnFizzAfterBonus(int input)
         {
             var expectedValue = "Fizz";
+
+            var actualValue = _fizzBuzz.BuildFizzBuzzString(input);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestCase(52)]
+        [TestCase(254)]
+        [TestCase(1757)]
+        [TestCase(5059)]
+        public void ReturnBuzzAfterBonus(int input)
+        {
+            var expectedValue = "Buzz";
+
+            var actualValue = _fizzBuzz.BuildFizzBuzzString(input);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestCase(53)]
+        [TestCase(30)]
+        [TestCase(57)]
+        [TestCase(135)]
+        [TestCase(315)]
+        [TestCase(31250)]
+        public void ReturnFizzBuzzAfterBonus(int input)
+        {
+            var expectedValue = "FizzBuzz";
 
             var actualValue = _fizzBuzz.BuildFizzBuzzString(input);
 
